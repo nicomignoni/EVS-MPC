f31ee2ba3671b23d1402ed9484229f405338e829
Nicola Mignoni <58893938+nicomignoni@users.noreply.github.com>
2022-04-02 12:43:51 +0200

    Delete qos_EJC.m

--- mpc/qos_EJC.m
+++
@@ -1,5 +0,0 @@
-function qos_EJC = qos_EJC(k, T, beta, gamma, K, d, r, b, Max, init)
-    qos_EJC = sum(beta(k).*((Max.b(k) - b)/K - r - sum(d)).^2) ...
-            - sum(gamma(k).*(b - [init.b.C(k) b(1:T-1)]));
-end
-
